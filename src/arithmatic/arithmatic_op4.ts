5ee41e1598defe5151445de43ac457b2677d92c1
sagnik.chatterjee3 <sagnik.chatterjee3@ibm.com>
2023-05-01 11:11:55 +0530

    Src: added arithmatic operator 4

---
+++ src/arithmatic/arithmatic_op4.ts
@@ -0,0 +1,10 @@
+//logical operators 
+// help us evaluate logic using boolean operators
+
+// && -> true if both the expression evaluate to true 
+// || -> true if at least one of them evlas to true 
+// ! -> true if false and vice versa
+
+console.log((10<20) || (20>10)); 
+console.log(!(10<45));
+console.log((10>5) && (5>3));
