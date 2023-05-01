4e0a097812b315cd7f68e5c98457ff377cc34a19
sagnik.chatterjee3 <sagnik.chatterjee3@ibm.com>
2023-05-01 10:43:01 +0530

    src: added till heterogeneous array

---
+++ notes/variables/notes.md
@@ -0,0 +1,12 @@
+- Variables define the data ; instead of requireing us to remember the location where it is stored.
+- Typescript provides out of the box support for classes and ES6 plus syntax.
+- Numbers :- can carry both integers and floats
+- String :- "" or ''
+- booolean of type true or false 
+- any for any type of data
+- enum is a set of constant values
+- In variables the variables are typed and there is strict checking.
+- naming conventions used as that in javascript.
+
+- Object Types :- using classes classes and interfaces.
+- 
---
+++ src/variables/variable1.ts
@@ -0,0 +1,44 @@
+let n1: number = 10;
+console.log(n1);
+
+//defining variables of certain types
+
+let s1: string = "some string";
+let s2: string = `This is another dynamic string ${s1}`;
+console.log(s2);
+
+//boolean type of variables 
+//useful for logic and conditional stmts 
+
+let bool1: boolean = true;
+if (bool1) {
+    console.log("Ok ");
+}
+
+//using any to add any data type to another 
+
+let any_item: any = {
+    productId: 1,
+    productName: 'phone',
+    productCost: 1000,
+};
+
+console.log(any_item);
+
+//homogeneous array type
+
+let arr1: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+arr1.forEach((item: number) => {
+    console.log(item);
+})
+//length of array
+console.log(arr1.length);
+
+//heterogeneous array :- different types of items in the ame 
+// can do as Array<any>
+
+let arr2: Array<any> = [1,null,false,"ok",{productName: 'nothing'}];
+
+arr2.forEach((item) => {
+    console.log(item);
+})
