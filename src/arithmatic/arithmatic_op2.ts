084630f6f9b6616bacfa515e7b56368293670d8a
sagnik.chatterjee3 <sagnik.chatterjee3@ibm.com>
2023-05-01 11:05:03 +0530

    src: added assignment op

---
+++ src/arithmatic/arithmatic_op2.ts
@@ -0,0 +1,9 @@
+//assignment operators 
+// assignment of the num1 
+let num1: number = 10;
+let num2: number = 2;
+let num3 = num2; //type inference
+console.log(num3);
+
+num3 += num2 ;
+console.log(num3);
