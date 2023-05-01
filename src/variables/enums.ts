ca229cceb30ae60b7d107485fe5c35a977170fd1
sagnik.chatterjee3 <sagnik.chatterjee3@ibm.com>
2023-05-01 10:50:17 +0530

    src: added enums

---
+++ src/variables/enums.ts
@@ -0,0 +1,16 @@
+// Enum or Enums are a collection of constant values 
+
+enum Gender {
+    Male,
+    Female,
+    Non_Binary,
+}
+
+//access the items as read only 
+
+console.log(Gender.Male);
+console.log(Gender.Non_Binary);
+console.log(Gender.Female);
+
+console.log(Gender[0]);
+console.log(Gender[2]);
