232ab070a137789ebd60350d74b7603e608c5cfa
sagnik.chatterjee3 <sagnik.chatterjee3@ibm.com>
2023-05-01 11:57:15 +0530

    src: destrutured an object

--- src/objects/objects1.ts
+++ src/objects/objects1.ts
@@ -1,9 +1,19 @@
 //objects in javacript have properties that have some some values.
 export let student = {
-    name:"John",
+    firstName:"John",
     age: 22,
     score: 85,
+    lastName: "Biley",
+
 };
 
 //to access the object using the dot notation 
-console.log(`Name: ${student.name} -- Age: ${student.age} -- Score :${student.score}`);
+console.log(`Name: ${student.firstName} -- Age: ${student.age} -- Score :${student.score}`);
+
+
+// adding destrucring the object 
+
+let {firstName,lastName} = student;
+
+console.log(firstName);
+console.log(lastName);
