4ba82dbfb31fd8963d6a8ef270a9c40f29235752
sagnik.chatterjee3 <sagnik.chatterjee3@ibm.com>
2023-05-02 11:42:39 +0530

    src: added inheritance using child component

--- src/inheritance/inteheritance1.ts
+++ src/inheritance/inteheritance1.ts
@@ -14,13 +14,33 @@ class BMW {
     stop(): void {
         console.log("Stop");
     }
+
+    constructor(make: string,model:string,year:string){
+        this.make = make;
+        this.year = year;
+        this.model = model;
+    }
 }
 
 class ThreeSeries extends BMW {
     //BMW come for free here 
     cruiseControlEnabled!: boolean;
+    constructor(make: string,model: string, year: string,cruiseControlEnabled: boolean){
+        //have to extend the parent class constructor using super()
+        super(make,model,year);
+        this.cruiseControlEnabled = cruiseControlEnabled;
+    }
 }
 
 class FiveSeries extends BMW {
     pakingAssistEnabled!: boolean;
+    constructor(make: string,model: string, year: string,parkingAssistEnabled: boolean){
+        //have to extend the parent class constructor using super()
+        super(make,model,year);
+        this.pakingAssistEnabled = parkingAssistEnabled;
+    }
 }
+
+
+let threeSeires = new ThreeSeries("Germany","i3","2018",false);
+console.log(threeSeires.cruiseControlEnabled);
