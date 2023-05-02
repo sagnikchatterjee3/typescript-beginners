685dc834a297f2574f55afdaeb54144b589a4f4e
sagnik.chatterjee3 <sagnik.chatterjee3@ibm.com>
2023-05-02 11:24:30 +0530

    added classes

--- src/classes/class1.ts
+++ src/classes/class1.ts
@@ -10,11 +10,36 @@ class Passenger {
 
 
     //adding methods for this class :- Function Properties
-    display(){
+    display() {
         console.log(`${this.firstName} ${this.lastName} ${this.frequentFlyerNo}`);
     }
 }
 
-let passenger1: Passenger = new Passenger("Rohan","Sharma",3456);
+let passenger1: Passenger = new Passenger("Rohan", "Sharma", 3456);
 
 passenger1.display();
+
+
+//using for in to display all object 
+for (let item in passenger1) {
+    console.log(item);
+    //console.log(passenger1[item]);
+}
+
+// display only properties not functions 
+//1.
+for (let item in passenger1) {
+    if (item !== "display") {
+        console.log(item);
+    }
+}
+
+//2. using the instance of operator
+for(let item in passenger1){
+    if(passenger1[item] instanceof Function){
+        continue; 
+    }
+    else {
+        //....
+    }
+}
